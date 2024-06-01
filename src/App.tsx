d84f5bf6503fa6aaa76869d0dfec431d19614461
Vadim Denisovich <vadimDenis0vich@yandex.com>
2024-06-01 12:27:12 +1000

    Added MessageBox.tsx and styleshhet

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import "./App.css";
-import Blank from "./components/blank/Blank";
-import Header from "./components/header/Header";
+import './App.css';
+import Blank from './components/blank/Blank';
+import Header from './components/header/Header';
 import PromptInput from "./components/promptInput/PromptInput";
 import Center from "./components/Center/Center";
+import MessageBox from "./components/message box/MessageBox";
 
 function App() {
   return (
-    <>
-      <Header />
-      <div className="App">
-        <Center>
-          <Blank>
-           asd
-          </Blank>
-          <PromptInput />
-        </Center>
-      </div>
-    </>
+  <>
+    <Header />
+    <div className="App">
+      <Center>
+      <Blank>
+        <MessageBox mode='Bot'/>
+        {/*<MessageBox mode='User'/>*/}
+      </Blank>
+      <PromptInput/>
+    </Center>
+    </div>
+  </>
   );
 }
 
---
+++ src/components/message box/MessageBox.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './MessageBox.css'
+
+interface MessageBoxProps {
+    mode: string;
+}
+
+const MessageBox: React.FC<MessageBoxProps> = ({ mode }) => {
+    const [text, setText] = React.useState('');
+    return (
+        <div className='{mode}'>
+            Это сообщение от {mode}
+        </div>
+    )
+}
+
+export default MessageBox
