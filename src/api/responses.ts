d27967d2caa48b5933fb2c90419e1456a820c360
4444urka <psinka2006@gmail.com>
2024-06-02 01:51:27 +1000

    new api update

--- src/api/responses.ts
+++ src/api/responses.ts
@@ -1,7 +1,11 @@
+import { Question } from "../types/questionResponse";
 import { $host } from "./index";
 
-export async function question(params:string) {
-    const response = $host.post('/question', params);
+export async function question(params: Question) {
+  try {
+    const response = await $host.post("/question", params);
     return response;
-
+  } catch (error) {
+    throw error;
+  }
 }
