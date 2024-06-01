dd8c8ebda66cbb0dc55dabacaa6cfb65bf88eaca
4444urka <psinka2006@gmail.com>
2024-06-02 09:37:59 +1000

    interface fix

--- src/api/responses.ts
+++ src/api/responses.ts
@@ -1,15 +1,10 @@
 import { Question } from "../types/questionResponse";
 import { $host } from "./index";
 
-// Определите тип данных для ответа сервера
-interface ServerResponse {
-  query: string;
-  // Добавьте другие поля, если они есть
-}
-
-export async function question(params: Question): Promise<ServerResponse> {
+export async function question(params: Question): Promise<any> {
   try {
-    const response = await $host.post<ServerResponse>("/question", params);
+    const response = await $host.post<any>("/assist", params);
+    console.log(response.data); // Выведите полный ответ сервера для отладки
     return response.data; // Доступ к данным ответа сервера через response.data
   } catch (error) {
     throw error; // Выбросить ошибку, если запрос завершился неудачей
